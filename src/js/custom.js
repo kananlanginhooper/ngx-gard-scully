820af86f962d040dcb88ede1273aabb6c947f764
artemnih <23387542+artemnih@users.noreply.github.com>
2020-12-03 18:38:21 -0500

    readme and script

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,8 +1,12 @@
-document.onreadystatechange = x => {
-    const alertButton = document.getElementById('alert-button');
-    if (alertButton) {
-        alertButton.addEventListener('click', () => {
-            alert('You clicked the button, this code is coming from custom.js')
-        })
+(() => {
+    let buttonFound = false;
+    document.onreadystatechange = x => {
+        const alertButton = document.getElementById('alert-button');
+        if (alertButton && !buttonFound) {
+            buttonFound = true;
+            alertButton.addEventListener('click', () => {
+                alert('You clicked the button, this code is coming from custom.js')
+            })
+        }
     }
-}
+})()
